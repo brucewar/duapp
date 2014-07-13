6443c24b67c6655e55df1cd43713921eeea0418b
brucewar <wjl891014@gmail.com>
2014-07-13 16:43:07 +0800

    add sitebar

--- controllers/site.js
+++ controllers/site.js
@@ -2,60 +2,80 @@ var models = require('../models');
 var ArticleClass = models.ArticleClass,
   Article = models.Article,
   Project = models.Project,
-  Comment = models.Comment;
+  Comment = models.Comment,
+  Blogroll = models.Blogroll;
 
 var utils = require('../libs/utils'),
   EventProxy = require('eventproxy'),
   config = require('../config').config;
 
-exports.index = function(req, res){
+exports.index = function(req, res) {
   var latest = 5;
-  var options = {limit: latest, sort: [['time', 'desc']]};
-  var render = function(articles){
-    res.render('index', {userName: req.session.userName, articles: articles});
+  var options = {
+    limit: latest,
+    sort: [
+      ['time', 'desc']
+    ]
+  };
+  var render = function(articles, blogrolls) {
+    res.render('index', {
+      userName: req.session.userName,
+      articles: articles,
+      blogrolls: blogrolls
+    });
   };
 
-  var proxy = EventProxy.create('articles', render);
-  proxy.fail(function(err){
-    if(err){
+  var proxy = EventProxy.create('articles', 'blogrolls', render);
+  proxy.fail(function(err) {
+    if (err) {
       return err;
     }
   });
 
-  Article.find({}, null, options, function(err, articles){
+  //获取最新5篇博文
+  Article.find({}, null, options, function(err, articles) {
     var classProxy = new EventProxy();
-    classProxy.after('class', articles.length, function(classes){
-      for(var i = 0, len = articles.length; i < len; i++){
-        if(classes[i]){
+    classProxy.after('class', articles.length, function(classes) {
+      for (var i = 0, len = articles.length; i < len; i++) {
+        if (classes[i]) {
           articles[i].class_name = classes[i].name;
         }
         articles[i].create_time = utils.formatDate(articles[i].time, 'yyyy-MM-dd');
       }
     });
-    for(var i = 0, len = articles.length; i < len ; i++){
-      ArticleClass.findOne({_id: articles[i].class_id}, classProxy.group('class'));
+    for (var i = 0, len = articles.length; i < len; i++) {
+      ArticleClass.findOne({
+        _id: articles[i].class_id
+      }, classProxy.group('class'));
     }
     //取评论数
     var commentProxy = new EventProxy();
-    commentProxy.after('comment_count', articles.length, function(comment_counts){
-      for(var i = 0, len = articles.length; i < len; i++){
+    commentProxy.after('comment_count', articles.length, function(comment_counts) {
+      for (var i = 0, len = articles.length; i < len; i++) {
         articles[i].comment_count = comment_counts[i];
       }
       proxy.emit('articles', articles);
     });
-    for(var j = 0; j < len ; j++){
-      Comment.count({article_id: articles[j]._id}, commentProxy.group('comment_count'));
+    for (var j = 0; j < len; j++) {
+      Comment.count({
+        article_id: articles[j]._id
+      }, commentProxy.group('comment_count'));
     }
+  });
+
+  //获取友情链接
+  Blogroll.find(function(err, blogrolls) {
+    proxy.emit('blogrolls', blogrolls);
   });
 };
 
-exports.showArticleContent = function(req, res){
+exports.showArticleContent = function(req, res) {
   var classId = req.query.class_id;
   var current_page = req.query.page || 1;
 
   var limit = config.page_limit;
 
-  var render = function(noClassCount, classes, articles, pages){
+  var render = function(noClassCount, classes, articles, pages) {
     res.render('content', {
       userName: req.session.userName,
       class_id: classId,
@@ -68,17 +88,17 @@ exports.showArticleContent = function(req, res){
   };
 
   var proxy = EventProxy.create('noClassCount', 'classes', 'articles', 'pages', render);
-  proxy.fail(function(err){
-    if(err){
+  proxy.fail(function(err) {
+    if (err) {
       return err;
     }
   });
 
   //获取总页数
-  switch(classId){
+  switch (classId) {
     case undefined:
-      Article.count(function(err, count){
-        if(err){
+      Article.count(function(err, count) {
+        if (err) {
           return err;
         }
         var pages = Math.ceil(count / limit);
@@ -86,17 +106,21 @@ exports.showArticleContent = function(req, res){
       });
       break;
     case '0':
-      Article.count({class_id: undefined}, function(err, count){
-        if(err){
+      Article.count({
+        class_id: undefined
+      }, function(err, count) {
+        if (err) {
           return err;
         }
         var pages = Math.ceil(count / limit);
         proxy.emit('pages', pages);
       });
       break;
-    default :
-      Article.count({class_id: classId}, function(err, count){
-        if(err){
+    default:
+      Article.count({
+        class_id: classId
+      }, function(err, count) {
+        if (err) {
           return err;
         }
         var pages = Math.ceil(count / limit);
@@ -106,57 +130,71 @@ exports.showArticleContent = function(req, res){
   }
 
   //获取未分类博文篇数
-  Article.count({class_id: undefined}, proxy.done('noClassCount'));
+  Article.count({
+    class_id: undefined
+  }, proxy.done('noClassCount'));
 
   //获取分类信息
-  ArticleClass.find(function(err, classes){
-    if(err){
+  ArticleClass.find(function(err, classes) {
+    if (err) {
       return err;
     }
     var classProxy = new EventProxy();
-    classProxy.after('count', classes.length, function(counts){
-      for(var i = 0, len = classes.length; i < len; i++){
+    classProxy.after('count', classes.length, function(counts) {
+      for (var i = 0, len = classes.length; i < len; i++) {
         classes[i].count = counts[i];
       }
       proxy.emit('classes', classes);
     });
 
-    for(var i = 0, len = classes.length; i < len; i++){
-      Article.count({class_id: classes[i]._id}, classProxy.group('count'));
+    for (var i = 0, len = classes.length; i < len; i++) {
+      Article.count({
+        class_id: classes[i]._id
+      }, classProxy.group('count'));
     }
   });
 
-  var options = {skip: (current_page - 1) * limit, limit: limit, sort: [['time', 'desc']]};
+  var options = {
+    skip: (current_page - 1) * limit,
+    limit: limit,
+    sort: [
+      ['time', 'desc']
+    ]
+  };
   //获取博文信息
-  switch(classId){
+  switch (classId) {
     case undefined:
-      Article.find({}, null, options, function(err, articles){
-        if(err){
+      Article.find({}, null, options, function(err, articles) {
+        if (err) {
           return err;
         }
-        articles.forEach(function(article){
+        articles.forEach(function(article) {
           article.create_time = utils.formatDate(article.time, 'yyyy-MM-dd hh:mm');
         });
         proxy.emit('articles', articles);
       });
       break;
     case '0':
-      Article.find({class_id: undefined}, null, options, function(err, articles){
-        if(err){
+      Article.find({
+        class_id: undefined
+      }, null, options, function(err, articles) {
+        if (err) {
           return err;
         }
-        articles.forEach(function(article){
+        articles.forEach(function(article) {
           article.create_time = utils.formatDate(article.time, 'yyyy-MM-dd hh:mm');
         });
         proxy.emit('articles', articles);
       });
       break;
-    default :
-      Article.find({class_id: classId}, null, options, function(err, articles){
-        if(err){
+    default:
+      Article.find({
+        class_id: classId
+      }, null, options, function(err, articles) {
+        if (err) {
           return err;
         }
-        articles.forEach(function(article){
+        articles.forEach(function(article) {
           article.create_time = utils.formatDate(article.time, 'yyyy-MM-dd hh:mm');
         });
         proxy.emit('articles', articles);
@@ -165,19 +203,24 @@ exports.showArticleContent = function(req, res){
   }
 };
 
-exports.showProjectList = function(req, res){
-  Project.find(function(err, projects){
-    if(err){
+exports.showProjectList = function(req, res) {
+  Project.find(function(err, projects) {
+    if (err) {
       return err;
     }
-    projects.forEach(function(project){
+    projects.forEach(function(project) {
       project.start_time = utils.formatDate(project.time, 'yyyy-MM');
       project.detail = project.detail ? project.detail.substr(0, 20) : '';
     });
-    res.render('project', {userName: req.session.userName, projects: projects});
+    res.render('project', {
+      userName: req.session.userName,
+      projects: projects
+    });
   });
 };
 
-exports.showAbout = function(req, res){
-  res.render('about', {userName: req.session.userName});
+exports.showAbout = function(req, res) {
+  res.render('about', {
+    userName: req.session.userName
+  });
 };
