d6d2e10a43cfafc0795481b2d69bcb02da210d0f
brucewar <wjl891014@gmail.com>
2014-08-19 14:29:46 +0800

    markdown project detail at backend

--- public/javascripts/project_detail.js
+++ public/javascripts/project_detail.js
@@ -1,8 +1,3 @@
-$(document).ready(function(){
-  var $detail = $('#project').find('div.inner');
-  var detail = $detail.html();
-  detail = detail.replace(/^\s+|\s+$/g, '');
-  var html = marked(detail);
-  $detail.html(html);
-  prettyPrint();
+$(document).ready(function() {
+	prettyPrint();
 });
